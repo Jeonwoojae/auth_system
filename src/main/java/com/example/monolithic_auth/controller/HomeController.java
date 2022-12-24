20cee107b8ead13deb15c76ac6e945f0c05d3faa
Jeonwoojae <dnwo0719@icloud.com>
2022-12-24 22:46:25 +0900

    [feat]서버 로그인 후 토큰 쿠키에 저장,

--- src/main/java/com/example/monolithic_auth/controller/HomeController.java
+++ src/main/java/com/example/monolithic_auth/controller/HomeController.java
@@ -1,36 +1,54 @@
 package com.example.monolithic_auth.controller;
 
+import com.example.monolithic_auth.domain.Users;
 import com.example.monolithic_auth.dto.UserRequestDto;
 import com.example.monolithic_auth.service.LoginService;
+import com.example.monolithic_auth.service.UserService;
+import com.example.monolithic_auth.utils.JwtProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @CrossOrigin(origins = "*")
 @RestController
+@Slf4j
 public class HomeController {
+    @Value("${jwt.expired-time}")
+    private String exp;
     private final LoginService loginService;
+    private final JwtProvider jwtProvider;
+    private final UserService userService;
 
     @Autowired
-    public HomeController(LoginService loginService){
+    public HomeController(LoginService loginService,UserService userService, JwtProvider jwtProvider){
         this.loginService = loginService;
-
+        this.jwtProvider = jwtProvider;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody final UserRequestDto params){
-        Map<String, Object> response = new HashMap<>();
+    public String login(
+            HttpServletResponse response,
+            @RequestBody final UserRequestDto params){
 
-        if(loginService.login(params)){
-            response.put("result","SUCCESS");
-            response.put("user",params.getEmail());
-        }else {
-            response.put("result", "FAIL");
-            response.put("reason", "일치하는 회원 정보가 없습니다.");
-        }
+        if(!loginService.login(params)) return null;
 
-        return response;
+        log.info("토큰 발급");
+        Users nowUser = userService.findByEmail(params.getEmail());
+        String token = jwtProvider.createToken(nowUser);
+
+        log.info("쿠키에 토큰 저장");
+        Cookie cookie = new Cookie("token",token);
+        cookie.setDomain("localhost");
+        cookie.setPath("/"); //모든 위치에서 저장
+        cookie.setMaxAge(Integer.parseInt(exp));
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+
+        return token;
     }
 }
